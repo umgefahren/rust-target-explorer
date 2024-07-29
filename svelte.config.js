9eef942e346445a8021bcbe0949210660b9ce95f
umgefahren <55623006+umgefahren@users.noreply.github.com>
2024-07-29 18:40:52 +0200

    Use cloudflare for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
-// import adapter from '@sveltejs/adapter-cloudflare'
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare'
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
